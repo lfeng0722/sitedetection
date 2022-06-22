17c007a0768ab2beb22d175ada0585a5922c83b4
“lfeng0722” <linfengliang722@gmail.com>
2022-06-22 20:06:02 +1000

    0622

--- flatness_costmap.py
+++ flatness_costmap.py
@@ -10,19 +10,19 @@ def flatness_detection(img_path):
     img_edges = cv2.Canny(img_gray, 1, 250)
 
     non_zero=[]
+    non_zero_2 = np.argwhere(img_edges != 0)
+
     for i in range(len(img_edges)):
         for j in range(len(img_edges[i])):
 
             if img_edges[i][j]!=0:
                 non_zero.append((i,j))
 
-    for m in range(np.shape(img_gray)[0]):
-        for n in range(np.shape(img_gray)[1]):
-            flat=[]
-            for tar in non_zero:
-                flat.append(math.sqrt(math.pow((m-tar[0]),2) + math.pow((n-tar[1]),2)))
-                # print(flat)
-            flatness[m][n]=min(flat)
+    ixs = np.ndindex(img_gray.shape[0],img_gray.shape[1])
+    for ix in ixs:
+        # print(ix)
+        flatness[ix[0]][ix[1]] = np.linalg.norm(np.array(ix)-non_zero_2, axis=1).min()
+
 
     heatmapshow = None
     heatmapshow = cv2.normalize(flatness, heatmapshow, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
--- main.py
+++ main.py
@@ -16,6 +16,10 @@ if __name__ == '__main__':
     # retval = cv2.imwrite('/home/linfeng/PycharmProjects/sitedetection/heatmap_3.jpg', heatmapshow)
     # cv2.imshow("Heatmap", heatmapshow)
     # cv2.waitKey(50)
-    cv2.imwrite('final_cost.jpg', final_costmap)
-    # cv2.imshow('final',final_costmap)
+    # cv2.imwrite('final_cost.jpg', final_costmap)
+    # print(final_costmap)
+    gray = cv2.cvtColor(final_costmap, cv2.COLOR_BGR2GRAY)
+    print(np.where(gray==np.max(gray)))
+
+    # cv2.imshow('final',gray)
     # cv2.waitKey(0)
