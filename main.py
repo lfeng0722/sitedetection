6cb1988f0f068252e52d0fa66294cc95d06488ff
“lfeng0722” <linfengliang722@gmail.com>
2022-06-30 11:26:06 +1000

    0630

--- main.py
+++ main.py
@@ -26,13 +26,20 @@ if __name__ == '__main__':
     gray = cv2.cvtColor(final_costmap, cv2.COLOR_BGR2GRAY)
     thresholding = np.argwhere(gray>210)
     # print(len(thresholding))
-    kmeans = KMeans(n_clusters=3).fit(thresholding)
+    kmeans = KMeans(n_clusters=10).fit(thresholding)
     # print(len(kmeans.labels_))
     # print(kmeans.cluster_centers_)
-    for coord in kmeans.cluster_centers_:
-        coord = coord.astype(int)
-        print(coord)
-        cv2.drawMarker(gray, position=coord, color=(0, 0, 255), markerSize=50, markerType=cv2.MARKER_CROSS,
+    cluster = []
+    for i in range(len(kmeans.cluster_centers_)):
+        # print("Cluster", i)
+        # print("Center:", kmeans.cluster_centers_[i])
+        # print("Size:", sum(kmeans.labels_ == i))
+        cluster.append(sum(kmeans.labels_ == i))
+    # for coord in kmeans.cluster_centers_:
+    #     coord = coord.astype(int)
+    #     # print(coord)
+    print(kmeans.cluster_centers_[np.argmax(cluster)],)
+    cv2.drawMarker(gray, position=kmeans.cluster_centers_[np.argmax(cluster)].astype(int), color=(255, 0, 0), markerSize=50, markerType=cv2.MARKER_CROSS,
                        thickness=5)
     cv2.imshow('final',gray)
     cv2.waitKey(0)
